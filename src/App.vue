33545adf6a5493745665c3b88d64e1f3c89cf3d0
andreacl2020 <68306235+andreacl2020@users.noreply.github.com>
2022-12-12 17:55:44 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+  <h2>peliculas</h2>
+  <Form />
+  <Button />
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+import Button from "./components/Button.vue";
+export default {
+  name: "App",
+  components: {
+    Form: Form,
+    Button: Button,
+  },
+};
+</script>
+
+<style lang ="scss">
+@import "./styles/_variables.scss";
+html,
+body {
+  background: $bg;
+  color: $color;
+  margin: 0;
+  font-family: sans-serif;
+}
+h2 {
+  border-bottom: 1px solid $color;
+  padding-bottom: 10px;
+}
+</style>
+
---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    type="button"
+    v-on:click="movie.quantity -= 1"
+    :disabled="movie.quantity <= 0"
+  >
+    -
+  </button>
+  {{ movie.quantity }}
+  <button
+    type="button"
+    v-on:click="movie.quantity += 1"
+    :disabled="movie.quantity >= movie.available"
+  >
+    +
+  </button>
+  -->
+</template>
+
+<style lang = "scss" scoped>
+@import "../styles/_variables.scss";
+button {
+  background: $color2;
+  border: 1px solid transparent;
+  cursor: pointer;
+  padding: 5px 10px;
+  transition: border-color 0.15;
+}
+button:hover {
+  border-color: $color2;
+}
+button[disabled] {
+  opacity: 0.5;
+}
+</style>
